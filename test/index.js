b0a90230b5f95de18fc9bc9031f07cf6f54adad5
nferocious76 <nferocious76@gmail.com>
2023-05-05 20:56:02 +0800

    Add test setup sample comments

--- test/index.js
+++ test/index.js
@@ -1,9 +1,15 @@
 const { SyncConfig, AsyncConfig } = require('../lib/index.js');
 const path = require('path');
 
+/**
+ * Using Sync configuration loading
+ */
 const syncConfg = path.resolve(__dirname + `/config2.json`);
 exports.syncConfig = SyncConfig({ path: syncConfg, debug: true });
 
+/**
+ * Using Async configuration loading
+ */
 const asyncConfg = path.resolve(__dirname + `/config.json`);
 exports.asyncConfig = async function () {
     return await AsyncConfig({ path: asyncConfg, debug: true })
